05c39a24c8293adcc3ecf25561ec5a9587db60ba
Andriy Matsievskiy <andriy.matsievskiy@upwave.net>
2018-09-24 12:17:05 +0300

    Added new map

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import MainMap from './map.png';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <img id="mapid" src="./or-osnova.png" />
+        <img id="mapid" src={ MainMap } />
       </div>
     );
   }
